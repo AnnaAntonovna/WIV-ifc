feb91e049d55b085c25e6d1389fff7cc84c682cd
Anna <a.baranova@strategiedigitali.io>
2023-06-14 12:22:49 +0200

    Load

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import { IfcViewerAPI } from 'web-ifc-viewer';
 
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+
+// Create grid and axes
 viewer.grid.setGrid();
 viewer.axes.setAxes();
 
 async function loadIfc(url) {
-    await viewer.IFC.setWasmPath("../../../");
+		// Load the model
     const model = await viewer.IFC.loadIfcUrl(url);
+
+		// Add dropped shadow and post-processing efect
     await viewer.shadowDropper.renderShadow(model.modelID);
+    
     viewer.context.renderer.postProduction.active = true;
 }
-loadIfc('DR_2SL_Elevator Sliding Door.ifc');
+loadIfc('./DR_2SL_Elevator Sliding Door.ifc');
